c45e03c00800d05964eb20618ec7c4ccf0965460
OrgestPasha <orgestpasha2005@gmail.com>
2024-09-22 18:33:34 +0200

    responsive navigation start

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,16 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { CommonModule,isPlatformBrowser } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
+
   
-  constructor(private router: Router) { }
+  
+  constructor(private router: Router,@Inject(PLATFORM_ID) private platformId: Object) { }
+
+
+  showNavigation:boolean=false;
+  showNavigationItems:boolean=false;
+  windowWidth:number=0;
+  ngOnInit() {
+    this.updateHeight();
+    if(isPlatformBrowser(this.platformId)){
+      this.windowWidth=window.innerWidth;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.updateHeight();
+    if(isPlatformBrowser(this.platformId)){
+      this.windowWidth=window.innerWidth;
+    }
+   
+  }
+
+  updateHeight() {
+    if (isPlatformBrowser(this.platformId)) {
+      if (window.innerWidth < 576) { // 36rem in pixels (assuming 1rem = 16px)
+        this.showNavigation=true;
+      } else {
+       this.showNavigation=false;
+      }
+    }
+  }
+
+  navigationToggle(){
+    this.showNavigationItems=!this.showNavigationItems;
+  }
+
 
   scrollToElement(id: string): void {
     this.router.navigate([], { fragment: id });
