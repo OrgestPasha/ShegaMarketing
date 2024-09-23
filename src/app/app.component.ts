7426e676d6c0505483c2b1396c522f822ea51044
OrgestPasha <orgestpasha2005@gmail.com>
2024-09-23 12:54:56 +0200

    dashboard and navbar completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavigationComponent],
+  imports: [RouterOutlet,NavigationComponent,DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
