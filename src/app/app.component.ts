b515017ba83a6a86825045f69ff7621ba0fb2e46
OrgestPasha <orgestpasha2005@gmail.com>
2024-09-23 14:45:14 +0300

    slogan start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SloganComponent } from './slogan/slogan.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavigationComponent,DashboardComponent],
+  imports: [RouterOutlet,NavigationComponent,DashboardComponent,SloganComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
