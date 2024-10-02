3d82c45f8cc770e732c72ffc0ae93d4ed506021e
OrgestPasha <orgestpasha2005@gmail.com>
2024-10-02 11:10:01 +0200

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { SloganComponent } from './slogan/slogan.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ServicesComponent } from './ServiceSector/services.component';
+import { ExtraComponent } from './extra/extra.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavigationComponent,DashboardComponent,SloganComponent,AboutUsComponent,ServicesComponent],
+  imports: [RouterOutlet,NavigationComponent,DashboardComponent,SloganComponent,AboutUsComponent,ServicesComponent,ExtraComponent,FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
